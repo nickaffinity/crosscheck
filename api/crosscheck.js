ee9da3f8f1a595de5582050068c4906f8fe46c2d
nickaffinity <nick@affinitywholehealth.com>
2025-04-28 13:14:11 -0400

    Update crosscheck.js

--- api/crosscheck.js
+++ api/crosscheck.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
         city: city,
         state: state,
         postalCode: postalCode,
-        country: country,
+        country: country
       })
     });
 
