6f7e9748a43f0f85d20c4f1fef7b8537f4ca5a8a
nickaffinity <nick@affinitywholehealth.com>
2025-04-28 13:10:48 -0400

    Update crosscheck.js

--- api/crosscheck.js
+++ api/crosscheck.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
 
-  const { first_name, last_name, email, phone, dob } = req.body;
+  const { first_name, last_name, email, phone, streetAddress, city, state, postalCode, country } = req.body;
 
   if (!first_name || !last_name || !email || !phone) {
     res.setHeader('Access-Control-Allow-Origin', '*');
